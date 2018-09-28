9e8581ae9e2bde1229880ab16b292194f606c8ae
unknown <dmitrijs.pavlovs123@gmail.com>
2018-09-28 14:29:54 +0300

    Modified image links for netify deployment

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.static(__dirname + '/public/'));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
+
 request('http://localhost:8084/', function(error, response, body) {
   w3c.validate(body, function(err) {
     console.error(err);
